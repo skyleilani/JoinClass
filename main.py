8a5e20d6056846cb77d838a3a398b218b167e21d
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-03-09 08:31:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-import pyautogui as pyg
+# PyAutoGUI lets your Python scripts control the 
+# mouse and keyboard to automate interactions with other applications.
+import pyautogui as pyauto
 import webbrowser as wb
 import datetime
 import time
 import click
 
 # translating date to python interperatable format 
+# should translate them to list format 
 def translate_date(date): 
-    dates = date.split(sep-"-")
+    dates = date.split("-",3)
     return list(map(int, dates))
 
 def translate_time(time):
-    times = time.split(sep="-")
+    times = time.split(":", 2)
+    print(list(map(int, times)))
     return list(map(int, times))
 
 def meeting_datetime(meeting_date, meeting_time):
-
     # (YY, MM, DD, HR, MIN, SEC)    
-    return datetime.datetime(meeting_date[2], meeting_date[1], meeting_date[0], meeting_time[0], meeting_time[1], meeting_time[2])
+    composed_datetime = datetime.datetime(meeting_date[2], meeting_date[1], meeting_date[0], meeting_time[0], meeting_time[1], meeting_time[2])
+   
+    return composed_datetime 
 
 # join the meeting 
+# meeting_link = str 
+# meeting_date = str
+# meeting_time = str
 
 def join_zoom (meeting_link, meeting_date, meeting_time): 
 
     translated_date = translate_date(meeting_date)
     translated_time = translate_time(meeting_time) 
-    meeting_datetime = meeting_datetime(translate_date, translated_time)
+    scheduled_meeting = meeting_datetime(translate_date, translated_time)
 
     # account for time difference between current time and meeting time 
-    time_til_meeting = (meeting_datetime - datetime.datetime.now().replace(microsecond=0)).total_seconds()
-    print("class starts in " + str(time_til_meeting/60) + " min")
+    time_til_meeting = (scheduled_meeting - datetime.datetime.now().replace(microsecond=0)).total_seconds()
+    print("class starts in " + str(time_til_meeting/60) + " minutes")
     time.sleep(time_til_meeting)
 
     # zoom plumbing 
@@ -38,10 +46,15 @@ def join_zoom (meeting_link, meeting_date, meeting_time):
 
     # open link on zoom app -- would like for this to be conditional for if app is installed 
     # & if no response for a certain amount of time, move on and open in browser 
-    pyg.click(x=805, y=254, clicks=1, interval=0, button='left')
+    pyauto.click(x=805, y=254, clicks=1, interval=0, button='left')
     time.sleep(10)
 
     # maximize zoom app 
-    pyg.click(x=195, y =31, clicks=1, interval=0, button='left')
+    pyauto.click(x=195, y =31, clicks=1, interval=0, button='left')
     time.sleep(3)
-    pyg.click(x=50, y=776, clicks=1, interval=0, button='left')
+    pyauto.click(x=50, y=776, clicks=1, interval=0, button='left')
+
+
+# join_zoom("https://zoom.us/j/5146739899?pwd=Z3JTZzZHd1lyMzRhcHJpemlLUDJOZz09", "3-8-2022", "9:12" )
+meeting_datetime("3-8-2022", "9:12")
+
